e0148948a97bc49f0aa92a108502ef3e3d6a3f0f
Ponkrishnan <136108406+Ponkrishnan@users.noreply.github.com>
2024-03-05 10:46:40 +0530

    Delete project2 directory

--- project2/project2app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import index
-
-class CustomerDetailForm(forms.ModelForm):
-    class Meta:
-        model = index
-        fields = ['name', 'email', 'subject', 'message']
